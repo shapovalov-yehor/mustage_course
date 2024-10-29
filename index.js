9928ec9ed5eaa1132fa192e4d73f85e100929025
dmitriypiskun <piskun.dmitriy@hotmail.com>
2024-10-29 14:14:50 +0200

    feat: add spinner block

--- index.js
+++ index.js
@@ -37,3 +37,136 @@ function moveToSlide(index) {
   sliderWrapper.style.transform = `translateX(${translateX}px)`;
   currentIndex = index;
 }
+
+const prizes = [
+  {
+    title: "Підписка на місяць безкоштовно",
+    color: "#1F1F1F",
+  },
+  {
+    title: "Проксі в подарунок",
+    color: "#6732D9",
+  },
+  {
+    title: "Інтерв'ю з Вусатим",
+    color: "#1F1F1F",
+  },
+  {
+    title: "Знижка 15% на розхідники",
+    color: "#6732D9",
+  },
+  {
+    title: "1 крео в подарунок",
+    color: "#1F1F1F",
+  },
+  {
+    title: "Доступ до приватного чату",
+    color: "#6732D9",
+  },
+  {
+    title: "+1 зідзвон з наставником",
+    color: "#1F1F1F",
+  },
+  {
+    title: "Акаунт 7-денного фарму в подарунок",
+    color: "#6732D9",
+  },
+];
+
+/**
+ * From: https://thecode.media/fortune/
+ */
+const wheel = document.querySelector(".deal-wheel");
+const spinner = wheel.querySelector(".spinner");
+const trigger = wheel.querySelector(".btn-spin");
+const ticker = wheel.querySelector(".ticker");
+
+const prizeSlice = 360 / prizes.length;
+const prizeOffset = Math.floor(180 / prizes.length);
+const spinClass = "is-spinning";
+const selectedClass = "selected";
+const spinnerStyles = window.getComputedStyle(spinner);
+
+let tickerAnim;
+let rotation = 0;
+let currentSlice = 0;
+let prizeNodes;
+
+const createPrizeNodes = () => {
+  prizes.forEach(({ title, color, reaction }, i) => {
+    const rotation = prizeSlice * i * -1 - prizeOffset;
+    spinner.insertAdjacentHTML(
+      "beforeend",
+      `<li class="prize" data-reaction=${reaction} style="--rotate: ${rotation}deg">
+        <span class="text">${title}</span>
+      </li>`
+    );
+  });
+};
+
+const createConicGradient = () => {
+  spinner.setAttribute(
+    "style",
+    `background: conic-gradient(
+      from 0deg,
+      ${prizes.map(({ color }, i) => `${color} 0 ${(100 / prizes.length) * (prizes.length - i)}%`).reverse()}
+    );`
+  );
+};
+
+const setupWheel = () => {
+  createConicGradient();
+  createPrizeNodes();
+  prizeNodes = wheel.querySelectorAll(".prize");
+};
+
+setupWheel();
+
+const spinertia = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const runTickerAnimation = () => {
+  const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
+  const a = values[0];
+  const b = values[1];
+  let rad = Math.atan2(b, a);
+
+  if (rad < 0) {
+    rad += 2 * Math.PI;
+  }
+
+  const angle = Math.round(rad * (180 / Math.PI));
+  const slice = Math.floor(angle / prizeSlice);
+
+  if (currentSlice !== slice) {
+    currentSlice = slice;
+  }
+};
+
+const selectPrize = () => {
+  const selected = Math.floor(Math.abs(rotation + 270) / prizeSlice) % prizes.length;
+  prizeNodes[selected].classList.add(selectedClass);
+};
+
+trigger.addEventListener("click", () => {
+  trigger.disabled = true;
+  rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
+  prizeNodes.forEach((prize) => prize.classList.remove(selectedClass));
+  wheel.classList.add(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  runTickerAnimation();
+});
+
+spinner.addEventListener("transitionend", () => {
+  cancelAnimationFrame(tickerAnim);
+  rotation %= 360;
+
+  selectPrize();
+
+  wheel.classList.remove(spinClass);
+  spinner.style.setProperty("--rotate", rotation);
+  trigger.disabled = false;
+});
